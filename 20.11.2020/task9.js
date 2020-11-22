be64e687fa4bef9112db6604a3a1349861261696
Lizarda <mirliz3748@gmail.com>
2020-11-23 01:05:06 +0200

    new version month

--- 20.11.2020/task9.js
+++ 20.11.2020/task9.js
@@ -1,13 +1,14 @@
 /*В переменной month хранится текущий месяц (new Date()).
 Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).*/
 let month = new Date();
-if(month.getMonth() == 11 || month.getMonth() == 0 || month.getMonth() == 1){
+let mon = month.getMonth();
+if(mon == 11 || mon == 0 || mon == 1){
     alert('Winter');
-} else if(month.getMonth() == 2 || month.getMonth() == 3 || month.getMonth() == 4){
+} else if(mon == 2 || mon == 3 || mon == 4){
     alert('Spring');
-} else if(month.getMonth() == 5 || month.getMonth() == 6 || month.getMonth() == 7){
+} else if(mon == 5 || mon == 6 || mon == 7){
     alert('Summer');
-} else if(month.getMonth() == 8 || month.getMonth() == 9 || month.getMonth() == 10){
+} else if(mon == 8 || mon == 9 || mon == 10){
     alert('Autumn');
 }
 
