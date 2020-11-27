ebe2b5d2e47f81415f1e9a2d95f71e5cd2a82626
Lizarda <mirliz3748@gmail.com>
2020-11-27 13:41:05 +0200

    errors

---
+++ 23.11.2020/task9.js
@@ -0,0 +1,12 @@
+/* Найдите ошибки
+for (let i = 0; i < 10; i++) {
+	for (let j = 0; j < 10; i++) {
+		console.log(i + j);
+	}
+}
+*/
+for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++) {
+        console.log(i + j);
+    }
+}
