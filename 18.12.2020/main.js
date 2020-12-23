af77091e5784756fd7bd36d6ffe48297178fadbd
lizarda09 <70509958+lizarda09@users.noreply.github.com>
2020-12-23 04:38:35 +0200

    Delete main.js

--- 18.12.2020/main.js
+++
@@ -1,6 +0,0 @@
-
-let ironMan = new Warrior('Iron Man',800, 80, 'img/man2.png' );
-let hulk = new Warrior('Hulk', 1000, 120, 'img/man3.png');
-let array = [];
-array.push(ironMan);
-array.push(hulk);
